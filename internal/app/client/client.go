45c599d2dfb520d5adfffff7edc61c89cb4065fe
azovcev <aaazovce@mts.ru>
2024-11-07 17:37:32 +0300

    remove debug [2]

--- internal/app/client/client.go
+++ internal/app/client/client.go
@@ -27,32 +27,7 @@ func NewApp(
 }
 
 func (app *App) Configure(ctx context.Context) error {
-
 	app.clientService = services.NewClientService(app.config.GRPC.Address, app.logger)
-
-	//db, err := postgres.NewPostgresql(
-	//	app.config.DB.Name,
-	//	app.config.DB.Host,
-	//	app.config.DB.Port,
-	//	app.config.DB.User,
-	//	app.config.DB.Password,
-	//	app.logger,
-	//)
-	//
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//userRepository := postgres.NewPostgresUserRepository(db)
-	//
-	//authService := services.NewAuthService(userRepository, app.config.Jwt.Secret, app.config.Jwt.Duration, app.logger)
-	//
-	//userService := services.NewUserService(userRepository, authService, app.logger)
-	//
-	//userService.Create(&command.CreateUserCommand{
-	//	Login:    "test",
-	//	Password: "123123",
-	//})
 
 	return nil
 }
